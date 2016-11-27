a2b97eee3620adfd2db0637745014cb564402e3d
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2016-11-27 15:40:22 -0300

    Interface Inicial

--- agenda-de-tarefas/agenda/js/funcoes.js
+++ agenda-de-tarefas/agenda/js/funcoes.js
@@ -6,6 +6,10 @@ var app = angular.module("agendaDeTarefas", []);
 
 app.controller("agendaDetarefasCtrl", function ($scope) {
 
-    $scope.nome = "Luiz";
-    $scope.sobrenome = "Fernando";
+    $scope.app = "Agenda de Tarefas";
+
+    $scope.tarefas = [
+        {tarefa: "Fazer o café da manhã", data: "12/05/1997-12:30"},
+        {tarefa: "Ir para a Universidade", data: "30/05/2000-13:30"}
+    ];
 });
