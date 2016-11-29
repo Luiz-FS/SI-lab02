76e6750fb041dc66c2f27a9d64fde32af719ae68
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2016-11-28 22:29:40 -0300

    Finalizando interface

--- agenda-de-tarefas/agenda/js/funcoes.js
+++ agenda-de-tarefas/agenda/js/funcoes.js
@@ -9,11 +9,20 @@ app.controller("agendaDetarefasCtrl", function ($scope) {
     $scope.app = "Agenda de Tarefas";
 
     $scope.tarefas = [
-        {tarefa: "Fazer o café da manhã", data: "12/05/1997-12:30"},
-        {tarefa: "Ir para a Universidade", data: "30/05/2000-13:30"}
+        {nome: "Fazer o café da manhã", data: "12/05/1997-12:30", concluida: false, deletar: false},
+        {nome: "Ir para a Universidade", data: "30/05/2000-13:30", concluida: false, deletar: false}
+    ];
+
+    $scope.tableFilter = [
+        {filtro: "Todas"},
+        {filtro: "Concluidas"},
+        {filtro: "Não concluidas"}
     ];
 
     $scope.adicionaTarefa = function (tarefa) {
+
+        tarefa.concluida = false;
+        tarefa.deletar = false;
         $scope.tarefas.push(angular.copy(tarefa));
         delete $scope.tarefa;
     }
@@ -37,8 +46,28 @@ app.controller("agendaDetarefasCtrl", function ($scope) {
             }
         });
 
-        var porcent = (sum/tarefas.length) * 100;
+        var porcent = 0;
+
+        if (tarefas.length > 0) {
+            porcent = (sum/tarefas.length) * 100;
+        }
 
         return Math.floor(porcent);
     }
+
+    $scope.filtroFunc = function (select) {
+
+        if (select == null || select.filtro == "Todos") return '';
+
+        if (select.filtro == "Concluidas") return true;
+        else if (select.filtro == "Não concluidas") return false;
+    }
+
+    $scope.isTarefaADeletar = function (tarefas) {
+
+        return tarefas.some(function (tarefa) {
+            return tarefa.deletar;
+        })
+    }
+
 });
